dc70e07df006a47861d6f6840a62947df3a448a1
Hugo López <hugo.lopez.carballo@iessanmamede.com>
2025-02-21 14:01:18 +0100

    Commit inicial

---
+++ src/main/java/com/ad/GestionCoches/GestionCochesApplication.java
@@ -0,0 +1,13 @@
+package com.ad.GestionCoches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCochesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCochesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ad/GestionCoches/GestionCochesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ad.GestionCoches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCochesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
